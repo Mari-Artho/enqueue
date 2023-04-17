d26317b7fd60edceb7add23519b3766289fd98c6
Mari-Artho <mari.artho@medieinstitutet.se>
2023-04-17 17:12:36 +0200

    Add Bookings.vue

---
+++ frontend/src/views/Bookings.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Alla bokningar</h1>
+    <md-card>
+      <md-card-header>
+        <h2>Alla bokningar</h2>
+      </md-card-header>
+    </md-card>
+  </div>
+</template>
