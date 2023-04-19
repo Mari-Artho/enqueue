a5f68d22219ca149589546d181cf1fcf879d6e6b
Mari-Artho <mari.artho@medieinstitutet.se>
2023-04-19 19:51:35 +0200

    Add all queues in bookings.vue

--- frontend/src/views/Bookings.vue
+++ frontend/src/views/Bookings.vue
@@ -1,65 +1,58 @@
 <template>
   <div>
-    <h1>Alla bokningar</h1>
     <md-card>
       <md-card-header>
-        <h2>Alla bokningar</h2>
+        <h2 class="md-title">Bokningar</h2>
       </md-card-header>
+
+      <md-card-content>
+        <md-table>
+          <md-table-row v-for="queue in queues" :key="queue.id" style="cursor: pointer" @click.native="open_queue(queue)">
+            <md-table-cell>
+              <md-icon v-if="!queue.open" class="md-accent"> lock </md-icon>
+              <md-icon v-else> lock_open </md-icon>
+              {{ queue.name }}
+            </md-table-cell>
+            <md-table-cell class="text-right">
+              {{ queue.queuing_count }}
+            </md-table-cell>
+          </md-table-row>
+        </md-table>
+      </md-card-content>
     </md-card>
-
-    <!-- Display when there is a booking -->
-    <md-table>
-      <h2>Bokad tid</h2>
-      <md-table-row>
-        <md-table-head> Tidslucka </md-table-head>
-
-        <md-table-head> Namn</md-table-head>
-
-        <md-table-head> Kommentar </md-table-head>
-
-        <md-table-head> Kommentar </md-table-head>
-
-        <md-table-head> Assisteras av </md-table-head>
-      </md-table-row>
-
-      <!-- <md-table-row
-        v-for="booking in queue.bookings"
-        :key="booking.id"
-        style="cursor: pointer"
-        :class="[
-          { studentIsHandled: booking.handlers.length > 0 },
-          {
-            myQueueRow: $store.state.profile !== null && booking.students.findIndex(x => x.id === $store.state.profile.id) !== -1,
-          },
-        ]"
-        @click="dialog_booking = booking"
-      >
-        
-        <md-table-cell>
-          {{ unix_to_datetime(booking.timestamp) }}
-          <br />
-
-        
-          <div v-if="booking.location !== null" :class="[{ badLocation: booking.bad_location }]">{{ booking.location }}</div>
-
-          <div v-else class="noLocation">ingen plats angiven</div>
-        </md-table-cell>
-
-        
-        <md-table-cell v-if="$store.state.profile !== null">
-          <div v-for="student in booking.students" :key="student.id">{{ student.name }}</div>
-        </md-table-cell>
-
-        
-        <md-table-cell>
-          <div v-if="booking.comment !== null">{{ booking.comment }}</div>
-        </md-table-cell>
-
-       
-        <md-table-cell>
-          <div>{{ booking.handlers.map(x => x.name + ' (' + x.user_name + ')').join(', ') }}</div>
-        </md-table-cell>
-      </md-table-row> -->
-    </md-table>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Queues',
+
+  data: () => ({
+    queues: [],
+  }),
+
+  async created() {
+    this.queues = (await axios.get('/queues')).data.sort((x, y) => {
+      if (x.open && !y.open) {
+        return -1
+      } else if (!x.open && y.open) {
+        return 1
+      } else if (x.name < y.name) {
+        return -1
+      } else if (x.name > y.name) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  },
+
+  methods: {
+    open_queue(queue) {
+      this.$router.push('/queues/' + queue.name)
+    },
+  },
+}
+</script>
