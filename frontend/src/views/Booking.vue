35c6259b8bd247937bd6d9b63477c2d909eaa6b8
Mari-Artho <mari.artho@medieinstitutet.se>
2023-04-20 19:58:42 +0200

    Create Booking.vue

---
+++ frontend/src/views/Booking.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>HELLO WORLD</h1>
+  </div>
+</template>
+
+<script>
+import Location from '../components/Location.vue'
+
+export default {
+  name: 'Booking',
+  //   components: {
+  //     Location,
+  //   },
+
+  data: () => ({
+    queue: null,
+  }),
+}
