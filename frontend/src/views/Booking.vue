91bce26cc05126d9e02854306329fa99133a79a5
Mari-Artho <mari.artho@medieinstitutet.se>
2023-04-28 20:19:44 +0200

    Fix fetching the right data for queue bookings.

--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -28,9 +28,10 @@
       </md-table-row>
 
       <!-- <md-table-row v-for="booking in queues.booking" :key="booking.id" style="cursor: pointer"> -->
-      <md-table-row>
+      <md-table-row v-for="booking in queue.bookings" :key="booking.id">
         <!-- Tid -->
-        <md-table-cell>hej</md-table-cell>
+        <!-- <md-table-cell>{{ unix_to_datetime(dialog_booking.timestamp) }}</md-table-cell> -->
+        <md-table-cell>{{ booking.id }}</md-table-cell>
 
         <!-- Plats -->
         <md-table-cell>hej</md-table-cell>
@@ -61,16 +62,32 @@ export default {
   data: () => ({
     queue: null,
     location: null,
+    dialog_booking: null,
   }),
   created() {
     this.$store.state.socket.on('connect', this.fetch_queue)
     this.fetch_queue()
   },
 
+  watch: {
+    dialog_queuing: function () {
+      this.dialog_booking = null
+    },
+
+    dialog_booking: function (n) {
+      this.dialog_queuing = null
+
+      if (n !== null) {
+        this.booking_location = n.location
+      }
+    },
+  },
+
   methods: {
     //get a que data
     fetch_queue() {
       fetch('/api/queues/' + this.$route.params.name)
+        //   fetch('/api/bookings/' + this.$route.params.name)
         .then(res => res.json())
         .then(queue => {
           this.queue = queue
@@ -83,11 +100,52 @@ export default {
         })
     },
 
+    sort_bookings() {
+      this.queue.bookings.sort((a, b) => {
+        if (a.timestamp < b.timestamp) {
+          return -1
+        } else if (a.timestamp > b.timestamp) {
+          return 1
+        } else {
+          if (a.id < b.id) {
+            return -1
+          } else if (a.id > b.id) {
+            return 1
+          } else {
+            return 0
+          }
+        }
+      })
+    },
+
     // create links from URLs that are embedded in text
     createLinks(text) {
       const urlRegex = /(https?:\/\/[^\s/$.?#]+\.[^\s]+)/g // regular expression to match URLs
       return text.replace(urlRegex, '<a href="$1">$1</a>') // replace URLs with HTML links
     },
+  },
+
+  //time
+  unix_to_datetime(unix) {
+    // TODO: övergå till något bibliotek, till exempel Moment
+    const d = new Date(unix)
+    const today = new Date()
+
+    const year = d.getFullYear()
+    const month = d.getMonth() + 1
+    const day = d.getDate()
+
+    const hour = '0' + d.getHours()
+    const min = '0' + d.getMinutes()
+    const time = hour.slice(-2) + ':' + min.slice(-2)
+    if (today.getDate() === d.getDate() && today.getMonth() === d.getMonth() && today.getFullYear() === d.getFullYear()) {
+      return `${year}/${month}/${day} ${time}`
+    }
+    var date = d.getDate() + ' ' + ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'][d.getMonth()]
+    if (today.getFullYear() !== d.getFullYear()) {
+      date += ' ' + d.getFullYear()
+    }
+    return date + ', ' + time
   },
 }
 </script>
