478bdcad33f76e0a57b1681a45ec67d0753e41ce
Mari-Artho <mari.artho@medieinstitutet.se>
2023-04-28 16:55:08 +0200

    Add Bokad tid table.

--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -2,5 +2,41 @@
   <div>
     <h1>HELLO WORLD</h1>
     <h2>You can do it! 😃🧸</h2>
+
+    <md-table>
+      <h2 style="margin-top: 3rem">Bokad tid</h2>
+      <!-- table-row -->
+      <md-table-row>
+        <md-table-head> Tidslucka </md-table-head>
+
+        <md-table-head> Plats </md-table-head>
+
+        <md-table-head> Namn</md-table-head>
+
+        <md-table-head> Kommentar </md-table-head>
+
+        <!-- <md-table-head> Kommentar </md-table-head> -->
+
+        <md-table-head> Assisteras av </md-table-head>
+      </md-table-row>
+
+      <!-- <md-table-row v-for="booking in queues.booking" :key="booking.id" style="cursor: pointer"> -->
+      <md-table-row>
+        <!-- Tid -->
+        <md-table-cell>hej</md-table-cell>
+
+        <!-- Plats -->
+        <md-table-cell>hej</md-table-cell>
+
+        <!-- Namn -->
+        <md-table-cell>hej</md-table-cell>
+
+        <!-- Kommentar -->
+        <md-table-cell>hej</md-table-cell>
+
+        <!-- Assisteras av -->
+        <md-table-cell>hej</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
