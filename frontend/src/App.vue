734ba2e90019c3f570df6b95d91a31c91fdba472
Mari-Artho <mari.artho@medieinstitutet.se>
2023-05-12 12:30:59 +0200

    Not only teachers but also students can see the Booking page.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
 
       <md-button v-if="$store.state.profile !== null && $store.state.profile.teacher" to="/admin"> <md-icon>build</md-icon> Administration </md-button>
 
-      <md-button v-if="$store.state.profile !== null && $store.state.profile.teacher" to="/bookings"> <md-icon>pending_actions</md-icon> Bokning </md-button>
+      <md-button v-if="$store.state.profile !== null" to="/bookings"> <md-icon>pending_actions</md-icon> Bokning </md-button>
 
       <md-button to="/test"><md-icon>local_florist</md-icon> Test Button</md-button>
 
